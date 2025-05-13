94cc5ec52eecf2c89cd8c59f67bf1121562e70d0
YTDev <belnisay@gmail.com>
2025-05-13 21:09:27 +0100

    adjt the card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -14,15 +14,17 @@ const Card = () => {
         </div>
       </div>
       <div className={styles.card_content}>
-        <div className={styles.card_label}>Product Reviews</div>
-        <div className={"${styles.card_title} h3"}>
+        <div className={`${styles.card_label} h6 mb-10 c-orange`}>
+          Product Reviews
+        </div>
+        <div className={"${styles.card_title} h3 mb-20"}>
           Lorem ipsum dolor sit amet, consectetur.
         </div>
-        <div className={styles.card_summary}>
+        <p className={styles.card_summary}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
           efficitur, nunc ut bibendum facilisis, nisi nisl aliquet nunc, eget
           tincidunt nisi nisl eget nunc.
-        </div>
+        </p>
       </div>
     </div>
   );
