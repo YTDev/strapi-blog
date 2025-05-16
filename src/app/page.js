b64fe200b96879c0886e5a85c01858f20da00fba
YTDev <belnisay@gmail.com>
2025-05-17 00:32:04 +0100

    make the card responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@ import Card from "@/components/card/Card";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="container pb-80">
       <Card />
+      <div className="row">
+        <div className="col col_4">
+          <Card />
+        </div>
+        <div className="col col_4">
+          <Card />
+        </div>
+        <div className="col col_4">
+          <Card />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,32 +4,34 @@ import Button, { IconTypes } from "../button/Button";
 
 const Card = () => {
   return (
-    <div className={styles.card}>
-      <div className={styles.card_imageWrap}>
-        <div className={styles.card_image}>
-          <Image
-            src="/thumb-featured-article.jpg"
-            alt="hero"
-            fill={true}
-            className={styles.card_image}
-          />
+    <div className={styles.card_wrap}>
+      <div className={styles.card}>
+        <div className={styles.card_imageWrap}>
+          <div className={styles.card_image}>
+            <Image
+              src="/thumb-featured-article.jpg"
+              alt="hero"
+              fill={true}
+              className={styles.card_image}
+            />
+          </div>
         </div>
-      </div>
-      <div className={styles.card_content}>
-        <div className={`${styles.card_label} h6 mb-10 c-orange`}>
-          Product Reviews
+        <div className={styles.card_content}>
+          <div className={`${styles.card_label} h6 mb-10 c-orange`}>
+            Product Reviews
+          </div>
+          <div className={"${styles.card_title} h3 mb-20"}>
+            Lorem ipsum dolor sit amet, consectetur.
+          </div>
+          <p className={styles.card_summary}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            efficitur, nunc ut bibendum facilisis, nisi nisl aliquet nunc, eget
+            tincidunt nisi nisl eget nunc.
+          </p>
+          <Button href="#" icon={IconTypes.ARROW_RIGHT}>
+            Read More
+          </Button>
         </div>
-        <div className={"${styles.card_title} h3 mb-20"}>
-          Lorem ipsum dolor sit amet, consectetur.
-        </div>
-        <p className={styles.card_summary}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          efficitur, nunc ut bibendum facilisis, nisi nisl aliquet nunc, eget
-          tincidunt nisi nisl eget nunc.
-        </p>
-        <Button href="#" icon={IconTypes.ARROW_RIGHT}>
-          Read More
-        </Button>
       </div>
     </div>
   );
